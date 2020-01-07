4ffb8258339b020a104f2b801b9c2ad4eb0e9169
Domenick Braccia <domenick.braccia@gmail.com>
2020-01-07 07:49:56 -0500

    updated kmergen.py

--- scripts/kmergen.py
+++ scripts/kmergen.py
@@ -6,6 +6,8 @@ import numpy as np
 A script to generate a user defined number of kmers of length 100
 '''
 
+# initializations & user input
+random.seed(50)
 bases = ['A', 'C', 'G', 'T']
 num_kmers = int(sys.argv[1])
 
@@ -17,6 +19,9 @@ def uniform_bernoulli_sequence(symbols, length):
 kmers = [None] * num_kmers 
 for n in np.arange(num_kmers):
     kmers[n] = uniform_bernoulli_sequence(bases, 100)
+
+# sainity check: all k-mers generated are unique
+print("out of", num_kmers, ":", len(set(kmers), "are unique."))
 
 # saving list of kmers to text file
 with open('../data/keyfile.txt', 'w') as filehandle:
