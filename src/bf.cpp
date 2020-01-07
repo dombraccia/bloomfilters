9ec5c532ce6d100734b5a166701b25dc56cabf3a
Domenick Braccia <domenick.braccia@gmail.com>
2020-01-07 07:49:20 -0500

    bloom filter is set up with all data members

--- src/bf.cpp
+++ src/bf.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sdsl/bit_vectors.hpp>
 #include "bloomfilter.h"
 using namespace std;
 
 // description
 int main(int argc, char** argv){
     // INITIALIZATIONS //
-    std::cout << argv[1] << " <-- [string] path to the key file" << endl;
+    // EXAMPLE CALL: ./tmp ../data/keyfile.txt 0.1 path2/
+    std::cout << argv[1] << " <-- [string] path to the key file" << "\n";
     string keyfile = argv[1]; // example: "../data/keyfile.txt"
     
-    std::cout << argv[2] << " <-- [float] false positive rate" << endl;
+    std::cout << argv[2] << " <-- [float] false positive rate" << "\n";
     float fdr = stof(argv[2]); // example: 0.1
 
     std::cout << argv[3] << " <-- [int] number of distinct keys" << endl;
-    int num_uniq_keys = stoi(argv[3]); // do we even need this? should be able to get from keyfile
+    int num_uniq_keys = stoi(argv[3]); // do we even need this? should just be 
+                                       // evaluated from keyfile.txt input
 
-    std::cout << argv[4] << " <-- [string] path to output file containing constructed bloom filter" << endl;
+    std::cout << argv[4] << " <-- [string] path to output file containing constructed bloom filter" << "\n";
     string bloomfile = argv[4]; // example: "../results/bloomfilter.x"
 
-    // K-MER GENERATION (step done by python script) //
+    // COUNTING NUMBER OF DISTINCT KEYS:
 
-
+    bloomfilter bf ( num_uniq_keys ,  fdr );
 };
--- src/bloomfilter.h
+++ src/bloomfilter.h
@@ -1,26 +1,48 @@
 /* bloomfilter.h */
 #include <iostream>
 #include <vector>
+#include <bitset>
+#include <include/sdsl/bit_vectors.hpp>
+#include <math.h>
 using namespace std;
-
-void calcParams(int num_keys, float FDR);
 
 // a file that defines and constructs a bloom filter
 class bloomfilter
 {
 public:
     // data members: size of filter in bits and number of hash functions to use
-    int size, hashes;
+    int num_keys;
+    double M, k; // because ceil outputs a double
+    float fdr;
+    sdsl::bit_vector bv;
+    vector<int> seeds;
 
     // constructor for the bloomfilter (m = size of bf in bits, k = # of hash functions to use)
-    bloomfilter(int m, int k)
+    bloomfilter(int num_keys, float fdr)
     {
-        // write somthing
-    }
-};
+        // calculate M and k based on input number of keys and FDR?
+        M = ceil (-1 * num_keys * log(fdr) / pow(log(2), 2));
+        k = ceil (M / num_keys) * log(2);
 
-// a function for calculating the size of filter and number of hash functions needed
-void calcParams(int num_keys, float FDR){
-    // int m = -1 * num_keys * ln(FDR) / (ln(2))^2
-    // k = (m / num_keys) * ln(2)
-}
+        // create bit_vector
+        bv = sdsl::bit_vector(M, 0);
+
+        // set hash functions
+        
+    };
+
+    void insert_key()
+    {
+        //
+    };
+
+    bool query()
+    {
+        //
+    };
+
+//private:
+    // Size of the bloom filter state in bits (2^16).
+	// static constexpr size_t bloomfilter_store_size = (-1) * num_keys * log(fdr) / (log(2))^2;
+    // bitset<bloomfilter_store_size> bv;
+};
